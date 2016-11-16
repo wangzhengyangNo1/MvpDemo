a93f709f42c03d682f38ad0bbcf21821f2c2187e
Wang Zhengyang <wzhyno1@163.com>
2016-11-16 17:30:00 +0800

    Find views.

--- app/src/main/java/com/wzhy/mvpdemo/ActivityLogin.java
+++ app/src/main/java/com/wzhy/mvpdemo/ActivityLogin.java
@@ -1,13 +1,52 @@
 package com.wzhy.mvpdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class ActivityLogin extends AppCompatActivity {
+
+    /*绑定控件*/
+    @BindView(R.id.edt_user_name)
+    AppCompatEditText mEdtUserName;
+    @BindView(R.id.edt_password)
+    AppCompatEditText mEdtPassword;
+    @BindView(R.id.btn_login)
+    AppCompatButton mBtnLogin;
+    @BindView(R.id.btn_clear)
+    AppCompatButton mBtnClear;
+    private Unbinder mUnbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mUnbinder = ButterKnife.bind(this);
+
+    }
+
+    @OnClick({R.id.btn_login, R.id.btn_clear})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+
+                break;
+            case R.id.btn_clear:
+
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        mUnbinder.unbind();
+        super.onDestroy();
     }
 }
